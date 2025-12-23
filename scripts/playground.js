f716a7dc6a5f2263bb34b3aba688844d7729bfa5
passarinho <k6lineon@gmail.com>
2025-12-22 23:22:40 -0300

    log e janelas de popup

--- scripts/playground.js
+++ scripts/playground.js
@@ -5,7 +5,47 @@ import { Map } from "./Map.js";
 console.log(animais);
 
 let gato = new Bicho(animais.gato_domestico);
-const p = document.getElementById('teste');
 let map = new Map(document.getElementById('map'), 30);
-map.draw(1800, 960);
-p.innerHTML = gato.speak();
+map.draw(1800, 960); // 60x32
+
+const logExpandButton = document.getElementById('log-expand');
+const logMinimizeButton = document.getElementById('log-minimize');
+const expandedLog = document.getElementById('expanded-log');
+const teste = document.getElementById('botao-window');
+
+logExpandButton.onclick = expandLog;
+logMinimizeButton.onclick = minimizeLog;
+teste.onclick = function() { addWindow('window', 20, 10); }
+
+function expandLog(event) {
+	expandedLog.style.display = 'flex';
+}
+
+function minimizeLog(event) {
+	expandedLog.style.display = 'none';
+}
+
+function addWindow(name, width, height) {
+	const windowPopUp = document.createElement('div');
+	const windowHeader = document.createElement('div');
+	const windowClose = document.createElement('div');
+	
+	windowPopUp.className = 'ui-layer';
+	windowPopUp.id = 'window-popup';
+	windowPopUp.style.width = width*30;
+	windowPopUp.style.height= height*30;
+	windowPopUp.style.right = ((60-width)/2)*30 + 120;
+	windowPopUp.style.bottom = ((32-height)/2)*30 + 120;
+	
+	windowHeader.className = 'ui-header texto';
+	windowHeader.innerHTML = name;
+	windowHeader.style.width = 'auto';
+
+	windowClose.className = 'close-button';
+	windowClose.innerHTML = '🞫';
+	windowClose.onclick = function() {windowPopUp.remove()}
+	
+	document.getElementById('ui').appendChild(windowPopUp);
+	windowPopUp.appendChild(windowHeader);
+	windowPopUp.appendChild(windowClose);
+}
